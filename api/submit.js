da80dc2f0c88aebf89993c7256475ad1ab5e5b15
Alex Wilmer <djazium@gmail.com>
2023-07-24 10:35:36 -0400

    oops forgot to save

--- api/submit.js
+++ api/submit.js
@@ -4,14 +4,14 @@ export default async function handler(request, response) {
   // use environment variable in production
   const resend = new Resend("re_VaqaoRda_E5ryF33kQGESmRPsj66RJfHu");
 
-  console.log(request.body);
+  const { body } = request;
 
   try {
     const data = await resend.emails.send({
       from: "onboarding@resend.dev",
       to: "djazium@gmail.com",
       subject: "New Invoice",
-      html: `<p>You can have received a new invoice from Company Name.</p>
+      html: `<p>You can have received a new invoice from ${body.companyName} for the amount $${body.total}.</p>
       <p>We don't store these invoices anywhere, so you can't pay them which is good because they're not real invoices anyways!`,
     });
     console.log(data);
