b891f503e8cfee17cc9ca1c246d88be3f6041ffd
Alex Wilmer <djazium@gmail.com>
2023-07-24 11:00:44 -0400

    loading state

--- api/submit.js
+++ api/submit.js
@@ -5,21 +5,14 @@ export default async function handler(request, response) {
   const resend = new Resend("re_VaqaoRda_E5ryF33kQGESmRPsj66RJfHu");
 
   const { body } = request;
-
-  let x = JSON.parse(body);
-
-  console.log(x.companyName);
-
-  // console.log(body);
-  // console.log(body.companyName);
-  // console.log(`Hi ${body.companyName}`);
+  const { companyName, total } = JSON.parse(body);
 
   try {
     const data = await resend.emails.send({
       from: "onboarding@resend.dev",
       to: "djazium@gmail.com",
       subject: "New Invoice",
-      html: `<p>You can have received a new invoice from ${body.companyName} for the amount $${body.total}.</p>
+      html: `<p>You can have received a new invoice from ${companyName} for the amount $${total}.</p>
       <p>We don't store these invoices anywhere, so you can't pay them which is good because they're not real invoices anyways!`,
     });
     console.log(data);
--- src/ReviewInvoiceModal.tsx
+++ src/ReviewInvoiceModal.tsx
@@ -1,4 +1,6 @@
+import { createSignal } from "solid-js";
 import { Stack, Box, Button, Modal, Typography } from "@suid/material";
+import { A } from "@solidjs/router";
 import useTheme from "@suid/material/styles/useTheme";
 
 export default function ReviewInvoiceModal({
@@ -9,7 +11,11 @@ export default function ReviewInvoiceModal({
 }) {
   const theme = useTheme();
 
+  const [loading, setLoading] = createSignal(false);
+  const [submitted, setSubmitted] = createSignal(false);
+
   async function submitInvoice() {
+    setLoading(true);
     const response = await fetch("/api/submit", {
       method: "POST",
       body: JSON.stringify({ ...state, total: total().toFixed(2) }),
@@ -17,8 +23,8 @@ export default function ReviewInvoiceModal({
         accept: "application/json",
       },
     });
-
-    console.log(response);
+    setLoading(false);
+    setSubmitted(true);
   }
 
   return (
@@ -41,19 +47,31 @@ export default function ReviewInvoiceModal({
           p: 4,
         }}
       >
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          Would you like to submit Invoice # {state.invoiceNumber} to{" "}
-          {state.targetEmail} for a total of ${total().toFixed(2)} due on{" "}
-          {state.dueDate}?
-        </Typography>
-        <Stack direction="row" spacing={3} mt={3}>
-          <Button onClick={submitInvoice} variant="contained">
-            Submit Invoice
-          </Button>
-          <Button onClick={handleClose} variant="contained" color="error">
-            Cancel
-          </Button>
-        </Stack>
+        {submitted() && (
+          <Box>
+            <p>Your invoice has been successfully submitted! </p>
+            <A href="/dashboard">
+              <Button variant="contained">Back to Dashboard</Button>
+            </A>
+          </Box>
+        )}
+        {!submitted() && (
+          <>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Would you like to submit Invoice # {state.invoiceNumber} to{" "}
+              {state.targetEmail} for a total of ${total().toFixed(2)} due on{" "}
+              {state.dueDate}?
+            </Typography>
+            <Stack direction="row" spacing={3} mt={3}>
+              <Button onClick={submitInvoice} variant="contained">
+                {loading() ? `Submitting` : `Submit Invoice`}
+              </Button>
+              <Button onClick={handleClose} variant="contained" color="error">
+                Cancel
+              </Button>
+            </Stack>
+          </>
+        )}
       </Box>
     </Modal>
   );
