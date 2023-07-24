9b5721b0c6713e0747f4559b90b6970a21bba843
Alex Wilmer <djazium@gmail.com>
2023-07-24 10:09:58 -0400

    wrap email send in try / catch

--- api/submit.js
+++ api/submit.js
@@ -1,14 +1,20 @@
 import { Resend } from "resend";
 
-export default function handler(request, response) {
-  const resend = new Resend(proces.env.RESEND_API_KEY);
+export default async function handler(request, response) {
+  // const resend = new Resend(process.env.RESEND_API_KEY);
+  const resend = new Resend("re_VaqaoRda_E5ryF33kQGESmRPsj66RJfHu");
 
-  resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: "djazium@gmail.com",
-    subject: "Hello World",
-    html: "<p>Congrats on sending your <strong>first email</strong>!</p>",
-  });
+  try {
+    const data = await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: "djazium@gmail.com",
+      subject: "Hello World",
+      html: "<p>Congrats on sending your <strong>first email</strong>!</p>",
+    });
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
 
   return response.send(`Hello world!`);
 }
